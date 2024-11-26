f641fb9b00363e981df143954fec7e334379e420
abolfazl8321 <abolfazlasgari629@gmail.com>
2024-11-25 22:32:57 -0800

    Assign an issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,39 +1,48 @@
 'use client';
-import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes'
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import {Skeleton} from '@/app/components';
+import { Skeleton } from "@/app/components";
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const AssigneeSelect = () => {
-    const {data:users,error,isLoading}=useQuery<User[]>({
-        queryKey:['users'],
-        queryFn:()=>axios.get('/api/users').then(res=>res.data),
-        staleTime:60*1000,
-        retry:3 //60s
-        
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+    const {
+      data: users,
+      error,
+      isLoading,
+    } = useQuery<User[]>({
+      queryKey: ["users"],
+      queryFn: () =>
+        axios.get("/api/users").then((res) => res.data),
+      staleTime: 60 * 1000, //60s
+      retry: 3
     });
-    if(isLoading) return <Skeleton/>
-        
-    if(error) return null;
-    useEffect(()=>{
-        const fetchUsers=async()=>{
-        const {data}=await axios.get<User[]>('/api/users');
-        }
-        fetchUsers();
-    },[]);
-  return (
-    <Select.Root>
-        <Select.Trigger aria-placegolder="Assign..."/>
-            <Select.Content>
-                <Select.Group>
-                    <Select.Label>Suggestions</Select.Label>
-                    {users?.map(user=>(<Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>))}
-                </Select.Group>
-            </Select.Content>
-    </Select.Root>
-  )
-}
+    if (isLoading) return <Skeleton />;
 
-export default AssigneeSelect
+    if (error) return null;
+  
+    return (
+        <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={(userId) => {
+          axios.patch("/api/issues/" + issue.id, {
+            assignedToUserId: userId || null,
+          });
+        }}
+      >
+        <Select.Trigger aria-placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+    )
+}
+export default AssigneeSelect;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,40 +1,43 @@
-import prisma from '@/prisma/client';
-import { Box,Flex,Grid } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import EditIssueButton from './EditIssueButton';
-import IssueDetail from './IssueDetail';
-import DeleteIssueButton from './DeleteIssueButton';
-import { getServerSession } from 'next-auth';
 import authOptions from '@/app/api/auth/[...nextauth]/authOptions';
+import IssueDetails from '@/app/issues/[id]/IssueDetail';
+import prisma from '@/prisma/client';
+import { Box, Flex, Grid } from '@radix-ui/themes';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
 import AssigneeSelect from './AssigneeSelect';
+import DeleteIssueButton from './DeleteIssueButton';
+import EditIssueButton from './EditIssueButton';
 
-interface Props{
-    params:{id:string}
+interface Props {
+  params: { id: string };
 }
 
-const IssueDetailPage =async ({params}:Props) => {
-    const session=await getServerSession(authOptions)
+const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
 
-    const issue=await prisma.issue.findUnique({
-        where:{id:parseInt(params.id)}
-    });
-    if(!issue) 
-      return notFound();
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
 
   return (
-    <Grid columns={{initial:"1",sm:"5"}} gap="5">
-      <Box className='lg:col-span-4'>
-        <IssueDetail issue={issue}/>
+    <Grid columns={{ initial: '1', sm: '5' }} gap="5">
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
       </Box>
-      {session && (<Box> 
-        <Flex direction="column" gap="4">
-          <AssigneeSelect/>
-        <EditIssueButton issueId={issue.id}/>
-        <DeleteIssueButton issueId={issue.id}/>
-        </Flex>
-      </Box>)}
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issue={issue}/>
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
