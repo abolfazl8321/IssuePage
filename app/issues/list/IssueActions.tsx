90e512cf847c606014ce8901343f4a3a15ab00f5
abolfazl8321 <abolfazlasgari629@gmail.com>
2024-11-27 03:51:44 -0800

    Filter issues

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,14 +1,14 @@
-import { Button, Flex } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
-import IssueFilter from './IssueFilter'
+import { Button, Flex } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+import IssueFilter from './IssueFilter';
 
 const IssueActions = () => {
   return (
-    <Flex mb='5' justify='between'>
-      <IssueFilter/>
-    <Button>
-      <Link href='/issues/new'>New Issues</Link>
+    <Flex mb="5" justify="between">
+      <IssueFilter />
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
       </Button>
   </Flex>
   )
